ecae4b7690e426f65ce277a2d43c3b4e50f0c661
Petko <limona.pb@gmail.com>
2019-07-07 00:20:03 +0300

    Encapsulation and exception handling exercise

---
+++ Person.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ShopingSpree
+{
+    public class Person
+    {
+        private string name;
+        private decimal money;
+        private List<Product> products;
+
+        public Person(string name, decimal money)
+        {
+            Name = name;
+            Money = money;
+            products = new List<Product>();
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            private set
+            {
+                if (value == string.Empty || value == null || value == " ")
+                {
+                    throw new ArgumentException("Name cannot be empty");
+                }
+                this.name = value;
+            }
+        }
+
+        public decimal Money
+        {
+            get
+            {
+                return this.money;
+            }
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Money cannot be negative");
+                }
+                this.money = value;
+            }
+        }
+
+        public IReadOnlyCollection<Product> Products
+        {
+            get
+            {
+                return this.products;
+            }
+        }
+
+        public void Buy(Product product)
+        {
+
+            if (this.money >= product.Cost)
+            {
+                products.Add(product);
+                Console.WriteLine($"{this.name} bought {product.Name}");
+                this.money -= product.Cost;
+            }
+            else
+            {
+                Console.WriteLine($"{this.name} can't afford {product.Name}");
+            }
+
+        }
+    }
+}
---
+++ StartUp.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ShopingSpree
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>();
+            string[] personas = Console.ReadLine()
+                .Split(';', StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < personas.Length; i++)
+            {
+                string[] currentPersonInfo = personas[i]
+                    .Split('=', StringSplitOptions.RemoveEmptyEntries);
+                try
+                {
+                    Person person = new Person(
+                        currentPersonInfo[0]
+                        , decimal.Parse(currentPersonInfo[1]));
+
+                    people.Add(person);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    return;
+                }
+            }
+            List<Product> products = new List<Product>();
+
+            string[] productsInfo = Console.ReadLine()
+                .Split(';', StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < productsInfo.Length; i++)
+            {
+                string[] currentProduct = productsInfo[i]
+                    .Split('=', StringSplitOptions.RemoveEmptyEntries);
+
+                try
+                {
+                    Product product = new Product(
+                        currentProduct[0],
+                        decimal.Parse(currentProduct[1]));
+
+                    products.Add(product);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    return;
+                }
+            }
+
+            string command = string.Empty;
+
+            while ((command = Console.ReadLine()) != "END")
+            {
+                string[] buyProduct = command
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                string buyerName = buyProduct[0];
+                string productToBuy = buyProduct[1];
+
+                Person person = people.FirstOrDefault(p => p.Name.Equals(buyerName));
+
+                Product product = products.FirstOrDefault(p => p.Name.Equals(productToBuy));
+
+                if (person != null && product != null)
+                {
+                    person.Buy(product);
+                }
+            }
+
+            foreach (var person in people)
+            {
+                if (person.Products.Count == 0)
+                {
+                    Console.WriteLine($"{person.Name} - Nothing bought ");
+                }
+                else
+                {
+                    Console.WriteLine($"{person.Name} - {string.Join(", ", person.Products)}");
+                }
+            }
+
+        }
+    }
+}
